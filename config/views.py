d22976b25b2be7f6b8dad91a2643d2282562a95c
ecruz91 <e.cruz@codelab.mx>
2016-07-14 19:57:13 -0500

    modificado:    config/views.py

--- config/views.py
+++ config/views.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib import auth
 from django.contrib.auth import logout, authenticate, login, get_user_model
-#from django.core.context_processors import csrf
 from django.template.context_processors import csrf
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from django.template.loader import get_template
 from django.contrib.auth.models import User, Group
 from .forms import UserLoginForm, UserRegister
+import subprocess, os
 
 def home(request):
 	#if request.user.is_authenticated() and request.user.is_active:
@@ -20,12 +20,18 @@ def home(request):
 
 def login_crm(request):
 	try:
-		group = Group.objects.get(name='administrador').user_set.all().count()
+		admins = Group.objects.get(name="administrador").user_set.all().count()
 	except Group.DoesNotExist:
-		group = 0
-	if group == 0:
+		admins = None
+	try:
+		users = Group.objects.get(name="usuario").user_set.all().count()
+	except Group.DoesNotExist:
+		users = None
+	print users
+	print admins
+	if admins == None or users == None or admins == 0:
 		return HttpResponseRedirect("/setup")
-	elif request.user.is_active:
+	elif (admins and users >= 0) and request.user.is_active:
 		return HttpResponseRedirect("/")
 	else:
 		form = UserLoginForm(request.POST or None)
@@ -45,8 +51,28 @@ def logout_crm(request):
 		return HttpResponseRedirect("/login/")
 
 def setup(request):
-	admins = Group.objects.get(name="administrador").user_set.all().count()
-	if admins == 0:
+	try:
+		admins = Group.objects.get(name="administrador").user_set.all().count()
+	except Group.DoesNotExist:
+		admins = None
+	try:
+		users = Group.objects.get(name="usuario").user_set.all().count()
+	except Group.DoesNotExist:
+		users = None
+	print users
+	print admins
+	if admins == None or users == None:
+		if(request.POST.get('mybtn')):
+			a = int(request.POST.get('mytextbox'))
+			if a == 1:
+				BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+				command = "python {}/manage.py loaddata initial_data.json".format(BASE_DIR)
+				process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+				output = process.communicate()[0]
+				print output
+				return HttpResponseRedirect("/login")
+		return render(request, 'auth/setup.html', {})
+	elif admins == 0 and users == 0:
 		reg_form = UserRegister(request.POST or None)
 		if reg_form.is_valid():
 			user = reg_form.save(commit=False)
@@ -58,8 +84,6 @@ def setup(request):
 		return render(request, 'auth/registro.html', {'reg_form':reg_form})
 	else:
 		return HttpResponseRedirect("/login")
-
-	
 
 def handler404(request):
 	response = render_to_response('404.html', {}, context_instance = RequestContext(request))
