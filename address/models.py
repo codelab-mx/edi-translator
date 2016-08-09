5a189ee06b7aaee5959894e5e1d1e32713e50272
ecruz91 <e.cruz@codelab.mx>
2016-08-09 10:49:50 -0500

    modificado:    address/models.py 	nuevo archivo: data_generator/migrations/0001_initial.py 	nuevo archivo: data_mining/migrations/0001_initial.py 	modificado:    data_mining/views.py 	modificado:    html/base.html 	modificado:    html/starter.html 	nuevo archivo: media/edi/28498 	nuevo archivo: media/edi/28498_TXKf2vH 	nuevo archivo: media/edi/EDI_830_2021 	nuevo archivo: sql/database.sqlite3

--- address/models.py
+++ address/models.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Company_Data(models.Model):
+	C_Prefix = models.CharField(max_length = 5)
+	C_Name = models.CharField(max_length = 100)
+	C_Address = models.CharField(max_length = 50)
+
+class Partner_Data(models.Model):
+	P_Prefix = models.CharField(max_length = 5)
+	P_Name = models.CharField(max_length = 100)
+	DUNS = models.CharField(max_length = 50)
+	P_Address = models.CharField(max_length = 50)
+	E_mail	= models.CharField(max_length = 100)
+	
+	
+
