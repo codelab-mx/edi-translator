1a4b5658fc77245eb85cf94fd96de1a8ad9ba5db
gonzard <g.zardain@codelab.mx>
2016-08-12 16:41:47 -0500

    modified:   settings.py 	new file:   ../site 	deleted:    ../static/admin/css/base.css 	deleted:    ../static/admin/css/changelists.css 	deleted:    ../static/admin/css/dashboard.css 	deleted:    ../static/admin/css/fonts.css 	deleted:    ../static/admin/css/forms.css 	deleted:    ../static/admin/css/login.css 	deleted:    ../static/admin/css/rtl.css 	deleted:    ../static/admin/css/widgets.css 	deleted:    ../static/admin/fonts/LICENSE.txt 	deleted:    ../static/admin/fonts/README.txt 	deleted:    ../static/admin/fonts/Roboto-Bold-webfont.woff 	deleted:    ../static/admin/fonts/Roboto-Light-webfont.woff 	deleted:    ../static/admin/fonts/Roboto-Regular-webfont.woff 	deleted:    ../static/admin/img/LICENSE 	deleted:    ../static/admin/img/README.txt 	deleted:    ../static/admin/img/calendar-icons.svg 	deleted:    ../static/admin/img/gis/move_vertex_off.svg 	deleted:    ../static/admin/img/gis/move_vertex_on.svg 	deleted:    ../static/admin/img/icon-addlink.svg 	deleted:    ../static/admin/img/icon-alert.svg 	deleted:    ../static/admin/img/icon-calendar.svg 	deleted:    ../static/admin/img/icon-changelink.svg 	deleted:    ../static/admin/img/icon-clock.svg 	deleted:    ../static/admin/img/icon-deletelink.svg 	deleted:    ../static/admin/img/icon-no.svg 	deleted:    ../static/admin/img/icon-unknown-alt.svg 	deleted:    ../static/admin/img/icon-unknown.svg 	deleted:    ../static/admin/img/icon-yes.svg 	deleted:    ../static/admin/img/inline-delete.svg 	deleted:    ../static/admin/img/search.svg 	deleted:    ../static/admin/img/selector-icons.svg 	deleted:    ../static/admin/img/sorting-icons.svg 	deleted:    ../static/admin/img/tooltag-add.svg 	deleted:    ../static/admin/img/tooltag-arrowright.svg 	deleted:    ../static/admin/js/SelectBox.js 	deleted:    ../static/admin/js/SelectFilter2.js 	deleted:    ../static/admin/js/actions.js 	deleted:    ../static/admin/js/actions.min.js 	deleted:    ../static/admin/js/admin/DateTimeShortcuts.js 	deleted:    ../static/admin/js/admin/RelatedObjectLookups.js 	deleted:    ../static/admin/js/calendar.js 	deleted:    ../static/admin/js/collapse.js 	deleted:    ../static/admin/js/collapse.min.js 	deleted:    ../static/admin/js/core.js 	deleted:    ../static/admin/js/inlines.js 	deleted:    ../static/admin/js/inlines.min.js 	deleted:    ../static/admin/js/jquery.init.js 	deleted:    ../static/admin/js/prepopulate.js 	deleted:    ../static/admin/js/prepopulate.min.js 	deleted:    ../static/admin/js/timeparse.js 	deleted:    ../static/admin/js/urlify.js 	deleted:    ../static/admin/js/vendor/jquery/LICENSE-JQUERY.txt 	deleted:    ../static/admin/js/vendor/jquery/jquery.js

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/https://codelab.mx/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = ''
